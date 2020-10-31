fb543bd49a34aeb30fe884e49297d5d97dedd5d5
ggmasterqwe <navid.fida@yahoo.com>
2020-10-31 16:49:42 +0330

    add: url test added

---
+++ urlShorter/TestsCases/urls_tests/test_models.py
@@ -0,0 +1,31 @@
+from django.test import TestCase
+from UrlShorterApp.urls.models import Url
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class UrlsModelTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(email='test@mail.com',
+                                        password='testpassword')
+
+    def test_create_url_with_slug(self):
+        data = {
+            'user':self.user,
+            'user_url': 'www.github.com/ggmasterqwe/url-shorter-drf',
+            'slug':'urksa'
+        }
+
+        url = Url.objects.create(**data)
+
+        self.assertAlmostEqual(url, Url.objects.get(**data))
+
+    def test_create_url_without_slug(self):
+        data = {
+            'user':self.user,
+            'user_url': 'www.github.com/ggmasterqwe/url-shorter-drf',
+        }
+
+        url = Url.objects.create(**data)
+        data['slug'] = url.slug
+        self.assertAlmostEqual(url, Url.objects.get(**data))
