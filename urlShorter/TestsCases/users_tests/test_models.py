134a4d94ba264797dd5fb11f25b829b21d1a12b5
ggmasterqwe <navid.fida@yahoo.com>
2020-10-31 16:50:15 +0330

    fix: user test fixed and admin added

--- urlShorter/TestsCases/users_tests/test_models.py
+++ urlShorter/TestsCases/users_tests/test_models.py
@@ -7,11 +7,10 @@ User = get_user_model()
 class UserAppModelTest(TestCase):
     def test_createing_user(self):
         data ={
-            'username': 'test_user',
             'password': 'testpassword',
             'email': 'test@mail.com'
         }
 
         user = User.objects.create(**data)        
 
-        self.assertEqual(user, User.objects.get(**data))
+        self.assertEqual(user, User.objects.get(email=data['email']))
