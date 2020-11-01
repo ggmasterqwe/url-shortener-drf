15a6e7876c5ede2237269548b5a163e0b76dd8ba
ggmasterqwe <navid.fida@yahoo.com>
2020-11-01 14:04:30 +0330

    fix: name issue

--- urlShortener/TestsCases/urls_tests/test_models.py
+++ urlShortener/TestsCases/urls_tests/test_models.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from UrlShorterApp.urls.models import Url
+from UrlShortenerApp.urls.models import Url
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
